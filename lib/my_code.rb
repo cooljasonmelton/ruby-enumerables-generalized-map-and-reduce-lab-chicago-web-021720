15ccc3975393429802755eb3e6ece8efcbe9cdc4
Jason Melton <jason.melton2@gmail.com>
2020-01-28 04:39:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,3 +5,14 @@ end
 map(array) do |my own map|
   
 end 
+
+
+def map_to_negativize(source_array)
+  array = []
+  i = 0 
+  while i < source_array.length do 
+    array << (source_array[i] * -1)
+    i += 1 
+  end 
+  array 
+end 
