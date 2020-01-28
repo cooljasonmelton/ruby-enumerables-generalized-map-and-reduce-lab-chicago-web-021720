a914d163ee1e04e299dc1828d083781b09379ef2
Jason Melton <jason.melton2@gmail.com>
2020-01-28 04:46:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,22 +8,3 @@ end
 
 
 
-
-[1,2,3,].map do |num|
-  num + 1 
-end 
-
-[1,2,3,].map {|num| num + 1} 
-
-yield()
-
-
-def map_to_negativize(source_array)
-  array = []
-  i = 0 
-  while i < source_array.length do 
-    array << (source_array[i] * -1)
-    i += 1 
-  end 
-  array 
-end 
