550d15ae9bf3e5994fb39f0d9f954e37d801b11f
Jason Melton <jason.melton2@gmail.com>
2020-01-28 04:57:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,12 @@
 def map(array)
   array = []
   i = 0 
-  
-  
+   while i < source_array.length do 
+    array << (source_array[i] * -1)
+    i += 1 
+  end 
+yield(array)
+array 
 end 
 
 def reduce(array)
