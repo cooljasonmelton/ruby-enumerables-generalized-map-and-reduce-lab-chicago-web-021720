541d3077fcb7ff5a5d2e2168e88b26942dee1ca2
Jason Melton <jason.melton2@gmail.com>
2020-01-28 05:11:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,7 +5,15 @@ def map(input)
     array << (yield(input[i]))
     i += 1 
    end 
-   
 array
 end
 
+def reduce(a, b = 0) 
+  total = b
+  i = 0 
+  while i < a.length do 
+    total += yield(a[i])
+    i += 1 
+  end 
+  total 
+end 
